92c8da2a81d97c642e9604f02c43e8cb96cc8243
Caio Ramos Ballarin <caioramosballarin@outlook.pt>
2025-04-10 11:43:11 -0300

    Adicionado pasta routes e arquivo dashboard.html, foi as conexoes com o influx

--- routes/rota_db.py
+++ routes/rota_db.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from influxdb_client import Point
-from db_conect import write_api, query_api
+from datetime import datetime, timedelta
+from fastapi.templating import Jinja2Templates
+from db_conect import write_api, query_api, client
 import os
 
 router = APIRouter()
 INFLUX_BUCKET = os.getenv("INFLUX_BUCKET")
+INFLUX_ORG = os.getenv("INFLUX_ORG")
+
+templates = Jinja2Templates(directory="template")
 
 #Criando as operações
 @router.post("/sensor")
@@ -15,7 +20,7 @@ def enviar_dados(cidade: str, temperatura: float, umidade: float):
         .field("temperatura", temperatura)
         .field("umidade", umidade)
     )
-    write_api.write(bucket=INFLUX_BUCKET, record=ponto)
+    write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=ponto)
     return {"mensagem": "Dado salvo com sucesso!"}
 
 @router.get("/sensor")
@@ -24,8 +29,9 @@ def consultar_dados():
         return {"mensagem": "BUCKET não definido"}
     
     query = f'''from(bucket: "{INFLUX_BUCKET}")
-        |> range(start: -1h)
+        |> range(start: -3h)
         |> filter(fn: (r) => r._measurement == "Leitura")
+        |> keep(columns: ["_time", "_field", "_value", "cidade"])
         '''
     
     result = []
@@ -39,3 +45,60 @@ def consultar_dados():
                 "valor": record.get_value()
             })
     return result
+
+@router.get("/cidades-validas")
+def listar_cidades_validas():
+    flux_query = f'''
+    from(bucket: "{INFLUX_BUCKET}")
+      |> range(start: -24h)
+      |> filter(fn: (r) => r._measurement == "Leitura")
+      |> group(columns: ["cidade"])
+      |> count()
+      |> keep(columns: ["cidade"])
+    '''
+    result = query_api.query(flux_query, org=INFLUX_ORG)
+    cidades = list({record.values.get("cidade") for table in result for record in table.records})
+    return cidades
+
+@router.get("/dashboard/{cidade}")
+def dashboard(request: Request, cidade: str):
+    dash_query = f'''
+    from(bucket= "{INFLUX_BUCKET}")
+        |> range(start: -30d)
+        |> filter( (r) => r._measurement == "Leitura" and r["cidade"] == "{cidade}")
+    '''
+    result = query_api.query(dash_query, org=INFLUX_ORG)
+
+    dados = []
+    for table in table:
+        for record in table.records:
+            dados.append({
+                "tempo": record.get_time(),
+                "campo": record.get_field(),
+                "valor": record.get_value()
+            })
+    
+    return templates.TemplateResponse("dashboard.html", {
+        "request": request,
+        "cidade": cidade,
+        "dados": dados
+    })
+
+@router.delete("/sensor")
+def deleta_dados(cidade: str):
+    delete_api = client.delete_api()
+
+    stop = datetime.utcnow()
+    start = stop - timedelta(days=1) # Ultimas 24 Horasr
+    
+    predicate = f'_measurement="Leitura" AND cidade="{cidade}"'
+
+    delete_api.delete(
+        start = start,
+        stop = stop,
+        predicate = predicate,
+        bucket = INFLUX_BUCKET, 
+        org = INFLUX_ORG
+        )
+
+    return{"mensagem": f"Dados da cidade '{cidade}' deletados com sucesso!"}
--- routes/site.py
+++ routes/site.py
@@ -1,58 +1,44 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from influxdb_client import QueryApi
+from db_conect import query_api
+import urllib.parse
+import os
 
 router = APIRouter()
 
 #Configurando os templates
 templates = Jinja2Templates(directory="template")
 
-#Colocando umas informações teste
-figuras = [
-   {
-        "id": 1,
-        "name": "Servidor 1",
-        "description": "A perfect circle with equal radius in all directions.",
-    },
-    {
-        "id": 2,
-        "name": "Servidor 2",
-        "description": "A four-sided polygon with equal sides and right angles.",
-    },
-    {
-        "id": 3,
-        "name": "Servidor 3",
-        "description": "A three-sided polygon with three angles.",
-    },
-    {
-        "id": 4,
-        "name": "Servidor 4",
-        "description": "A six-sided polygon with six angles.",
-    },
-    {
-        "id": 5,
-        "name": "Servidor 5",
-        "description": "A quadrilateral with all sides equal in length.",
-    },
-    {
-        "id": 6,
-        "name": "Servidor 6",
-        "description": "A five-sided polygon with five angles.",
-    }
-]
+INFLUX_BUCKET = os.getenv("INFLUX_BUCKET")
+INFLUX_ORG = os.getenv("INFLUX_ORG")
 
 #Usando o Request para converter a resposta de saida para html puro usando o response_class
 @router.get("/", response_class=HTMLResponse)
 async def exibir_interface(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "figuras": figuras})
+    flux_query = f'''
+    from(bucket: "{INFLUX_BUCKET}")
+      |> range(start: -30d)
+      |> filter(fn: (r) => r._measurement == "Leitura")
+      |> group(columns: ["cidade"])
+      |> count()
+      |> keep(columns: ["cidade"])
+    '''
 
-@router.get("/figuras", response_class=HTMLResponse)
-async def lista_figuras(request: Request):
-    return templates.TemplateResponse("item_list.html", {"request": request, "figuras": figuras})
+    result = query_api.query(flux_query, org=INFLUX_ORG)
+    cidades = list({record.values.get("cidade") for table in result for record in table.records})
 
-@router.get("/figura/{item_id}", response_class=HTMLResponse)
-async def detalhe_figura(request: Request, item_id: int):
-    fig_verif = next((f for f in figuras if f["id"] == item_id), None)
-    if fig_verif is None:
-        return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
-    return templates.TemplateResponse("item_detail.html", {"request": request, "figura": fig_verif})
+    figuras = []
+     
+    for idx, cidade in enumerate(cidades, start=1):
+        figuras.append({
+            "id": idx,
+            "name": cidade,
+            "name_encoded": urllib.parse.quote(str(cidade), safe=''),
+            "description": f"Dashboard de sensores da cidade {cidade}."
+        })
+
+    return templates.TemplateResponse("index.html", {
+        "request": request, 
+        "figuras": figuras})
